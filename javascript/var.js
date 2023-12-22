51768cb50a58e0b7ea422d88a31e9ef0480d8f46
MUHAMMAD SHAHAJAD <77879032+shahajad-id@users.noreply.github.com>
2023-12-22 20:03:45 +0530

    Update var.js

--- javascript/var.js
+++ javascript/var.js
@@ -2,4 +2,6 @@ const accountid=1234
 let accountmail="abc.com"
 accountcity="greater noida"
 var accountpass="1234"
-console.table([accountid,accountmail,accountcity,accountpass])
+let rollno;
+console.table([accountid,accountmail,accountcity,accountpass,rollno])
+//rollnow will show undefined
